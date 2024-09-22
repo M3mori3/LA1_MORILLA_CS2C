a4948e7db36078633e1707df453d90611d6917e5
M3mori3 <nylmorilla@gmail.com>
2024-09-22 21:36:40 +0800

    initial commit with activity.js

---
+++ activity1.js
@@ -0,0 +1,9 @@
+//Variables activity
+var name = "Lynmuel";
+var whatDoYouWannaBecomeInYourLife = "programmer";
+var gender = "female";
+var twitterHandle = "@_rirah";
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
